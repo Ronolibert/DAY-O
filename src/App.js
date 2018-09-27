980c9108b92c50d3d3cfb5f550380a2c8ba81c8f
Ron Cruz <Ronolibert@thecoursekey.com>
2018-09-26 21:07:58 -0700

    Be able to go to previous and next month

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const CalendarHeader = () => (
-  <div
-    style={{ display: 'flex', justifyContent: 'space-around', maxWidth: 1550 }}
-  >
-    {days.map(day => (
-      <h3>{day}</h3>
-    ))}
+const CalendarHeader = ({ currentMonth, onNextMonth, onPrevMonth }) => (
+  <div style={{ maxWidth: 1550 }}>
+    <h2
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginLeft: 95
+      }}
+    >
+      <button onClick={onPrevMonth}>PREV</button>
+      {months[currentMonth]}
+      <button onClick={onNextMonth}>NEXT</button>
+    </h2>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around'
+      }}
+    >
+      {days.map(day => (
+        <h3>{day}</h3>
+      ))}
+    </div>
   </div>
 );
 
 const weeks = [0, 1, 2, 3, 4];
 
 class App extends Component {
-  state = {};
+  state = {
+    currentMonth: 0
+  };
+
+  componentDidMount() {
+    this.setState({ currentMonth: new Date().getMonth() });
+  }
+
+  handlePrevMonth = () => {
+    const newMonth = this.state.currentMonth - 1;
+    if (newMonth >= 0) {
+      this.setState({ currentMonth: newMonth });
+    }
+  };
+
+  handleNextMonth = () => {
+    const newMonth = this.state.currentMonth + 1;
+    if (newMonth <= 11) {
+      this.setState({ currentMonth: newMonth });
+    }
+  };
 
   render() {
+    const { currentMonth } = this.state;
     return (
       <div>
         <div>
-          <CalendarHeader />
+          <CalendarHeader
+            currentMonth={currentMonth}
+            onNextMonth={this.handleNextMonth}
+            onPrevMonth={this.handlePrevMonth}
+          />
           <div style={{ marginLeft: 95 }}>
             {weeks.map(week => (
               <div style={{ display: 'flex' }}>
